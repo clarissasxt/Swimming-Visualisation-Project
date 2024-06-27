3e02f06c5cb278cc6f7cf947a2f227ec91fe5618
clarissasxt <clarissaseah68@gmail.com>
2024-06-27 10:47:31 +0800

    now displays 2 graphs with the video at the bottom

--- mediapipe_utils.py
+++ mediapipe_utils.py
@@ -4,8 +4,10 @@ import cv2
 import numpy as np
 from mediapipe.framework.formats import landmark_pb2
 from mediapipe import solutions
-
 import logging
+from threading import Thread, Lock
+import time
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
@@ -20,6 +22,7 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 MODEL_TYPE = 'lite'
 MODEL_PATH = f'pose_landmarker_{MODEL_TYPE}.task'
 MODE = VisionRunningMode.VIDEO
+
 options = PoseLandmarkerOptions(
         # base_options=BaseOptions(model_asset_path=MODEL_PATH, delegate=BaseOptions.Delegate.GPU),
         base_options=BaseOptions(model_asset_path=MODEL_PATH, delegate=BaseOptions.Delegate.CPU),
@@ -28,69 +31,90 @@ options = PoseLandmarkerOptions(
         min_pose_detection_confidence=0.1)
 
 def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
+    pose_landmarks_list = detection_result.pose_landmarks
+    annotated_image = np.copy(rgb_image)
+    
+    if not pose_landmarks_list:
+        return annotated_image
 
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
+    # Loop through the detected poses to visualize.
+    for idx in range(len(pose_landmarks_list)):
+        pose_landmarks = pose_landmarks_list[idx]
 
     # Draw the pose landmarks.
     pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
     pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
     ])
     solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
+        annotated_image,
+        pose_landmarks_proto,
+        solutions.pose.POSE_CONNECTIONS,
+        solutions.drawing_styles.get_default_pose_landmarks_style())
+    return annotated_image
 
 
-def write_pose_video(inp_video, csv_file):
-    fieldnames = ['0', '1', '2']
+def write_pose_video(inp_video, csv_file, out_video, frame_lock, current_frame):
+    fieldnames = ['15', '16', '13', '14']
     with open(csv_file, 'w') as file:
         csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
         csv_writer.writeheader()
 
     cap = cv2.VideoCapture(inp_video)
     fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_time = 1.0 / fps
 
     w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(out_video, fourcc, fps, (w, h))
 
     frames = []
     with PoseLandmarker.create_from_options(options) as landmarker:
         frame_i = 0
         while cap.isOpened():
+            start_time = time.time()
             ret, frame = cap.read()
             if ret:
-                frames.append(frame)
+                #frames.append(frame)
                 mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
     
                 # VIDEO
                 pose_landmarker_result = landmarker.detect_for_video(mp_image, int(frame_i / fps * 1000))
+                
+                # Draw landmarks on frame
+                annotated_image = draw_landmarks_on_image(frame, pose_landmarker_result)
+                out.write(annotated_image)
+
+                with frame_lock:
+                    current_frame[0] = annotated_image
+                
                 with open(csv_file, 'a') as file:
                     csv_writer  = csv.DictWriter(file, fieldnames=fieldnames)
                     if len(pose_landmarker_result.pose_landmarks):
                         info = {
-                            '0': int(pose_landmarker_result.pose_landmarks[0][0].x*w),
-                            '1': int(pose_landmarker_result.pose_landmarks[0][1].x*w),
-                            '2': int(pose_landmarker_result.pose_landmarks[0][2].x*w)
+                            '15': int(pose_landmarker_result.pose_landmarks[0][15].x*w),
+                            '16': int(pose_landmarker_result.pose_landmarks[0][16].x*w),
+                            '13': int(pose_landmarker_result.pose_landmarks[0][13].x*w),
+                            '14': int(pose_landmarker_result.pose_landmarks[0][14].x*w),
                         }
                     else:
                         info = {
-                            '0': None, '1': None, '2': None
+                            '15': None, '16': None, '13': None, '14': None
                         }
                     csv_writer.writerow(info)
                     logging.info(info)
 
                 frame_i = frame_i + 1
+                elapsed_time = time.time() - start_time
+                sleep_time = max(0, frame_time - elapsed_time)
+                time.sleep(sleep_time)
 
             else:
                 break
         cap.release()
+        out.release()
         cv2.destroyAllWindows()
 
 
--- stream_plot.py
+++ stream_plot.py
@@ -4,41 +4,69 @@ import pandas as pd
 from mediapipe_utils import write_pose_video
 import threading
 import numpy as np
-
+import cv2
 
 plt.style.use('fivethirtyeight')
 
-def animate(i, csv_file):
+def animate(i, csv_file, ax1, ax2, ax3, frame_lock, current_frame):
     data = pd.read_csv(csv_file)
 
     x = np.arange(len(data))
-    y0 = data['0']
-    y1 = data['1']
-    y2 = data['2']
+    left_wrist_y = data['15'] # left wrist
+    right_wrist_y = data['16'] # right wrist
+    left_elbow_y = data['13']
+    right_elbow_y = data['14']
 
-
-    plt.cla()
-    plt.plot(x, y0, label='y0')
-    plt.plot(x, y1, label='y1')
-    plt.plot(x, y2, label='y2')
+    # plt.cla()
+    # plt.plot(x, y0, label='y0')
+    # plt.plot(x, y1, label='y1')
+    # plt.plot(x, y2, label='y2')
+    
+    ax1.clear()
+    ax1.plot(x, left_wrist_y, label='Left wrist', linewidth=1.5)
+    ax1.plot(x, right_wrist_y, label='Right wrist', linewidth=1.5)
+    ax1.set_title("Wrist Movements", fontsize = 14)
+    ax1.legend()
+    
+    ax2.clear()
+    ax2.plot(x, left_elbow_y, label='Left elbow', linewidth=1.5)
+    ax2.plot(x, right_elbow_y, label='Right elbow', linewidth=1.5)
+    ax1.set_title("Elbow Movements", fontsize = 14)
+    ax2.legend()
+    
+    with frame_lock:
+        if current_frame[0] is not None:
+            ax3.clear()
+            ax3.imshow(cv2.cvtColor(current_frame[0], cv2.COLOR_BGR2RGB))
+            ax3.axis('off')
+            ax3.set_title("Live Camera", fontsize = 14)
+            
 
 class ExitCommand(Exception):
-
     pass
 
 
 if __name__ == '__main__':
     VIDEO_FILE = 'DJI_0883.MP4'
     CSV_FILE = 'pose.csv'
+    OUT_VIDEO_FILE = 'output_with_landmarks.mp4'
 
+    current_frame = [None]
+    frame_lock = threading.Lock()
 
-    t1 = threading.Thread(target=write_pose_video, args=(VIDEO_FILE, CSV_FILE))
+    t1 = threading.Thread(target=write_pose_video, args=(VIDEO_FILE, CSV_FILE, OUT_VIDEO_FILE, frame_lock, current_frame))
     t1.start()
 
-    ani = FuncAnimation(plt.gcf(), animate, fargs=(CSV_FILE,), interval=1000)
+    fig = plt.figure(figsize=(12, 8))
+
+    ax1 = plt.subplot2grid((2, 2), (0, 0))
+    ax2 = plt.subplot2grid((2, 2), (0, 1))
+    ax3 = plt.subplot2grid((2, 2), (1, 0), colspan=2)
+
+    ani = FuncAnimation(fig, animate, fargs=(CSV_FILE, ax1, ax2, ax3, frame_lock, current_frame), interval=100)
     plt.tight_layout()
     plt.show()
-    plt.legend()
+    #plt.legend()
 
     t1.join()
 
